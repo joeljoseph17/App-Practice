b6806503c630b4beb22a14e8130b305670a90927
Joel Joseph <joeljose@usc.edu>
2019-02-13 09:03:53 -0800

    Final Calculator Logic

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +47,80 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
+  double prev = 0;
+  double curr = 0;
+  String operator = '+';
+  int decimalOffset = 0;
+
+  onNumTap(int num) {
+    setState(() {
+      if(decimalOffset == 0) {
+        this.curr = this.curr * 10 + num;
+      } else {
+        this.curr = this.curr + num * pow(10, -decimalOffset);
+        ++this.decimalOffset;
+      }
+    });
+  }
+
+  onDecimalTap() {
+    setState(() {
+      if(this.decimalOffset == 0) {
+        this.decimalOffset = 1;
+      }
+    });
+  }
+
+  onOperatorTap(String operator) {
+    setState(() {
+      this.operator = operator;
+      this.prev = this.curr;
+      this.curr = 0;
+      this.decimalOffset = 0;
+    });
+  }
+
+  onClearTap() {
+    setState(() {
+      this.prev = 0;
+      this.curr = 0;
+      this.operator = '+';
+      this.decimalOffset = 0;
+    });
+  }
+
+  onEqualTap() {
+    setState((){
+      switch(this.operator) {
+        case '+':
+          this.curr = this.prev + this.curr;
+          break;
+        case '-':
+          this.curr = this.prev - this.curr;
+          break;
+        case '*':
+          this.curr = this.prev * this.curr;
+          break;
+        case '/':
+        default:
+          this.curr = this.prev / this.curr;
+          break;
+      }
+    });
+  }
+
+  onNegateTap() {
+    setState(() {
+      this.curr *= -1;
+    });
+  }
+
+  onPercentTap() {
+    setState(() {
+      this.curr /= 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -66,7 +140,7 @@ class MainPageState extends State<MainPage> {
                     children: <Widget>[
                       FittedBox(
                         child: Text(
-                          '0',
+                          this.curr.toString(),
                           style: TextStyle(
                             fontSize: 80,
                             color: Colors.white,
@@ -86,34 +160,16 @@ class MainPageState extends State<MainPage> {
                       child: Row(
                         children: <Widget>[
                           Expanded(
-                            child: Button(bgColor: this.widget.utilColor, textColor: this.widget.utilTextColor, text: "C", textSize: 30),
+                            child: Button(bgColor: this.widget.utilColor, textColor: this.widget.utilTextColor, text: "C", textSize: 30, onTap: onClearTap),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.utilColor, textColor: this.widget.utilTextColor, text: "+/-", textSize: 30),
+                            child: Button(bgColor: this.widget.utilColor, textColor: this.widget.utilTextColor, text: "+/-", textSize: 30, onTap: onNegateTap),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.utilColor, textColor: this.widget.utilTextColor, text: "%", textSize: 30),
+                            child: Button(bgColor: this.widget.utilColor, textColor: this.widget.utilTextColor, text: "%", textSize: 30, onTap: onPercentTap),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor,  text: "÷", textSize: 40),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "7", textSize: 35),
-                          ),
-                          Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "8", textSize: 35),
-                          ),
-                          Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "9", textSize: 35),
-                          ),
-                          Expanded(
-                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "×", textSize: 40),
+                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor,  text: "÷", textSize: 40, onTap: () => onOperatorTap('/')),
                           ),
                         ],
                       ),
@@ -122,16 +178,16 @@ class MainPageState extends State<MainPage> {
                       child: Row(
                         children: <Widget>[
                           Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "4", textSize: 35),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "7", textSize: 35, onTap: () => onNumTap(7)),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "5", textSize: 35),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "8", textSize: 35, onTap: () => onNumTap(8)),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "6", textSize: 35),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "9", textSize: 35, onTap: () => onNumTap(9)),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "-", textSize: 40),
+                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "×", textSize: 40, onTap: () => onOperatorTap('*')),
                           ),
                         ],
                       ),
@@ -140,16 +196,34 @@ class MainPageState extends State<MainPage> {
                       child: Row(
                         children: <Widget>[
                           Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "1", textSize: 35),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "4", textSize: 35, onTap: () => onNumTap(4)),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "2", textSize: 35),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "5", textSize: 35, onTap: () => onNumTap(5)),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "3", textSize: 35),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "6", textSize: 35, onTap: () => onNumTap(6)),
                           ),
                           Expanded(
-                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "+", textSize: 40),
+                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "-", textSize: 40, onTap: () => onOperatorTap('-')),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "1", textSize: 35, onTap: () => onNumTap(1)),
+                          ),
+                          Expanded(
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "2", textSize: 35, onTap: () => onNumTap(2)),
+                          ),
+                          Expanded(
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "3", textSize: 35, onTap: () => onNumTap(3)),
+                          ),
+                          Expanded(
+                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "+", textSize: 40, onTap: () => onOperatorTap('+')),
                           ),
                         ],
                       ),
@@ -159,15 +233,15 @@ class MainPageState extends State<MainPage> {
                         children: <Widget>[
                           Flexible(
                             flex: 2,
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "0", textSize: 30),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: "0", textSize: 30, onTap: () => onNumTap(0)),
                           ),
                           Flexible(
                             flex: 1,
-                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: ".", textSize: 30),
+                            child: Button(bgColor: this.widget.numColor, textColor: this.widget.numTextColor, text: ".", textSize: 30, onTap: onDecimalTap),
                           ),
                           Flexible(
                             flex: 1,
-                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "=", textSize: 40),
+                            child: Button(bgColor: this.widget.opColor, textColor: this.widget.opTextColor, text: "=", textSize: 40, onTap: onEqualTap),
                           ),
                         ],
                       ),
@@ -189,31 +263,36 @@ class Button extends StatelessWidget {
   final Color textColor;
   final double textSize;
   final String text;
+  final void Function() onTap;
 
   Button({Key key,
     this.bgColor,
     this.textColor,
     this.text,
     this.textSize,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build (BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(1000),
-        color: this.bgColor,
-      ),
-      child: Center(
-        child: Text(
-          this.text,
-          style: TextStyle(
-            fontSize: this.textSize,
-            color: this.textColor,
-            fontWeight: FontWeight.w200,
+    return GestureDetector(
+      onTap: this.onTap,
+      child: Container(
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(1000),
+          color: this.bgColor,
+        ),
+        child: Center(
+          child: Text(
+            this.text,
+            style: TextStyle(
+              fontSize: this.textSize,
+              color: this.textColor,
+              fontWeight: FontWeight.w200,
+            ),
           ),
         ),
       ),
